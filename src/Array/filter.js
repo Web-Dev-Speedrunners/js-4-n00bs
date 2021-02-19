fe919be25b988a7ed08dbfe61cfa5006d46c172f
Asif Shikder <shikdernyc@gmail.com>
2021-02-18 20:18:04 -0500

    Added Filter Function

--- src/Array/filter.js
+++ src/Array/filter.js
@@ -1,4 +1,8 @@
-// TODO: Refactor to not use array method
-Array.prototype.myFilter = function(filterFn) {
-  return this.filter(filterFn)
-}
+Array.prototype.myFilter = function (filterFn) {
+  const filteredArr = [];
+  for (let i = 0; i < this.length; i++) {
+    const entry = this[i];
+    if (filterFn(entry, i, this)) filteredArr.push(entry);
+  }
+  return filteredArr
+};
--- src/array.test.js
+++ src/array.test.js
@@ -1,10 +1,26 @@
 import './Array'
 
-describe("Runs simple tests", () => {
-  it("Tests filter", () => {
+describe("Test filter function", () => {
+  it("Able to filter using given function", () => {
     const inputArray = [1, 2, 3, 4, 5]
-    const expectedOutput = [2, 4]
     const filterOdd = (num) => num % 2 === 0
-    expect(inputArray.myFilter(filterOdd)).toEqual(expectedOutput)
+    const expectedArr = inputArray.filter(filterOdd)
+    expect(inputArray.myFilter(filterOdd)).toEqual(expectedArr)
+  })
+
+  it("Tests index param", () => {
+    const inputArray = [1, 2, 3, 4, 5]
+    const filterOddIndexes = (_, index) => index % 2 == 0
+    const expectedArr = inputArray.filter(filterOddIndexes)
+    expect(inputArray.myFilter(filterOddIndexes)).toEqual(expectedArr)
+  })
+
+  it("Returns instance array on each call", () => {
+    const inputArray = [1, 2, 3, 4, 5]
+    const mockFilterFN = jest.fn()
+    inputArray.myFilter(mockFilterFN)
+    expect(mockFilterFN).toHaveBeenCalledTimes(inputArray.length)
+    expect(mockFilterFN).toHaveBeenCalledWith(expect.anything(), expect.anything(), inputArray)
   })
 })
+

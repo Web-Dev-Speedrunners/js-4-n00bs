0d3b4ff825406e3913719889455771d01c766c24
Xuejin <Xuejingao@gmail.com>
2021-02-25 15:55:20 -0500

    Modified indexOf for NaN detection

--- src/Array/index_of.js
+++ src/Array/index_of.js
@@ -1,6 +1,6 @@
 Array.prototype.myIndexOf = function (searchItem, startIndex = 0) {
   for(let i = startIndex; i < this.length; i++){
-    if(this[i] === undefined || this[i] === NaN) continue;
+    if(this[i] === undefined || isNaN(this[i])) continue;
     if(this[i] === searchItem) return i;
   }
   return -1;
